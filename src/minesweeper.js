3e1c396248d7e5069019d42b51f32198c38cbf70
grosenb <grosenb@cisco.com>
2018-03-20 16:07:01 +1000

    update to use a nested array to represent the gameboard and add a function to print the current board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,19 @@
-// This is what a blank line would look like
-const blankLine = '  |   |  ';
-// Print out an empty board
-console.log('This is what an empty board would look like:');
-for (let i = 0; i < 3 ; i++) {
-  console.log(blankLine);
-}
-// This is what a line would look like with a guess on it
-const guessLine = '1 |   |  ';
-// This is what a line with a bomb on it would look like
-const bombLine = '  | B |  ';
-// Print out a board with a guess and a bomb on it
-console.log('This is what a board with a guess and a bomb on it would look like');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
