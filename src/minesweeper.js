9a5906c7c6bdd36bf11e660425cc9e066a62c7ef
grosenb <grosenb@cisco.com>
2018-03-26 10:47:34 +1000

    add conditional to ensure only one mine per square, add get number of neighbour bombs function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -21,13 +21,40 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   }
   numberOfBombsPlaced = 0;
   while (numberOfBombsPlaced < numberOfBombs) {
-    randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    // need to update to ensure bombs aren't place on top of existing bombs
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++;
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
   }
   return board;
+};
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = offset[0] + rowIndex;
+    const neighborColumnIndex = offset[1] + columnIndex;
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
 };
 
 const printBoard = board =>
